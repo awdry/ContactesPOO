4de91b86c400041a5a0e0581a039e6ac4438e0fa
Awdry Nolasco <nolascoawdry@gmail.com>
2026-03-21 20:53:30 -0400

    Agregar clase Contact con propiedades y constructores

---
+++ Contact.cs
@@ -0,0 +1,28 @@
+public class Contact
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string Address { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public int Age { get; set; }
+    public bool IsBestFriend { get; set; }
+
+    public string FullName => Name + " " + LastName;
+
+    public Contact() { }
+
+    public Contact(int id, string name, string lastName, string address,
+                   string phone, string email, int age, bool isBestFriend)
+    {
+        Id = id;
+        Name = name;
+        LastName = lastName;
+        Address = address;
+        Phone = phone;
+        Email = email;
+        Age = age;
+        IsBestFriend = isBestFriend;
+    }
+}
